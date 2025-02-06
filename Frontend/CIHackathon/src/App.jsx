535dbf46dedec4893f39a66b3b8c038261382768
Abhishek Thakre <abhishekpthakre2023@gmail.com>
2025-02-06 09:51:25 +0530

    login page

--- Frontend/CIHackathon/src/App.jsx
+++ Frontend/CIHackathon/src/App.jsx
@@ -1,11 +1,12 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./Components/Home"; 
+import Login from "./Components/Login";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Login />} />
       </Routes>
     </Router>
   );
